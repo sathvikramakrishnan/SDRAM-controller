ebd2b0116927638a808b72fde4a4b337a7eb7c5b
sathvik <sathvikr2004@gmail.com>
2026-02-26 04:09:45 +0530

    self refresh module

---
+++ sdram_self_ref.v
@@ -0,0 +1,146 @@
+`timescale 1ns / 1ps
+
+module sdram_self_ref_gen (
+    input sys_clk,
+    input sys_reset_n,
+    input init_done,
+    input sref_en,
+
+    output reg sdram_cke,
+    output reg [3:0] sref_cmd_out,
+    output reg [1:0] sref_bank_out,
+    output reg [11:0] sref_addr_out,
+    output sref_end
+);
+
+    // Command definitions
+    localparam CMD_NOP = 4'b0111;
+    localparam CMD_PRECHARGE = 4'b0010;
+    localparam CMD_AUTO_REF = 4'b0001;
+
+    // FSM parameters and signals
+    parameter 
+        SREF_IDLE = 4'd0,
+        SREF_PRECHARGE = 4'd1,
+        SREF_WAIT_TRP = 4'd2,
+        SREF_AUTO_REF = 4'd3,
+        SREF_WAIT_TRFC = 4'd4,
+        SREF_ENTER = 4'd5,
+        SREF_EXIT = 4'd6,
+        SREF_WAIT_TXSR = 4'd7,
+        SREF_END = 4'd8;
+
+    reg [3:0] sref_state;
+
+    // Waiting time parameters and signals
+    parameter 
+        TRP_COUNT = 2'd2,
+        TRFC_COUNT = 3'd7,
+        TXSR_COUNT = 4'd8;
+    
+    wire trp_end, trfc_end, txsr_end;
+    reg [2:0] wait_count;
+    reg wait_count_rst;
+
+    assign trp_end = ((sref_state == SREF_WAIT_TRP) & (wait_count == TRP_COUNT - 1'b1));
+    assign trfc_end = ((sref_state == SREF_WAIT_TRFC) & (wait_count == TRFC_COUNT - 1'b1));
+    assign txsr_end = ((sref_state == SREF_WAIT_TXSR) & (wait_count == TXSR_COUNT - 1'b1));
+
+    // Wait time counter
+    always @(posedge sys_clk or negedge sys_reset_n) begin
+        if (~sys_reset_n)
+            wait_count <= 2'd0;
+        else if (wait_count_rst)
+            wait_count <= 2'd0;
+        else
+            wait_count <= wait_count + 1'b1;
+    end
+
+    always @(*) begin
+        case (sref_state)
+            SREF_WAIT_TRP: wait_count_rst = trp_end;
+            SREF_WAIT_TRFC: wait_count_rst = trfc_end;
+            SREF_WAIT_TXSR: wait_count_rst = txsr_end;
+            SREF_EXIT: wait_count_rst = 1'b1;
+            default: wait_count_rst = 1'b1;
+        endcase
+    end
+
+    // FSM state transitions
+    always @(posedge sys_clk or negedge sys_reset_n) begin
+        if (~sys_reset_n) begin
+            sref_state <= SREF_IDLE;
+
+            sdram_cke <= 1'b1;
+            sref_cmd_out <=  CMD_NOP;
+            sref_bank_out <= 2'b11;
+            sref_addr_out <= 12'hfff;
+        end
+
+        else begin            
+            case (sref_state)
+                SREF_IDLE: begin
+                    if (init_done & sref_en)
+                        sref_state <= SREF_PRECHARGE;
+                end
+
+                SREF_PRECHARGE: begin
+                    sref_cmd_out <= CMD_PRECHARGE;
+                    sref_state <= SREF_WAIT_TRP;
+                end
+
+                SREF_WAIT_TRP: begin
+                    if (trp_end)
+                        sref_state <= SREF_AUTO_REF;
+
+                    else
+                        sref_cmd_out <= CMD_NOP;
+
+                end
+
+                SREF_AUTO_REF: begin
+                    sdram_cke <= 1'b0;
+                    sref_cmd_out <= CMD_AUTO_REF;
+                    sref_state <= SREF_WAIT_TRFC;
+                end
+
+                SREF_WAIT_TRFC: begin
+                    sref_cmd_out <= CMD_NOP;
+                    if (trfc_end)
+                        sref_state <= SREF_ENTER;
+                end
+
+                SREF_ENTER: begin
+                    if (~sref_en)
+                        sref_state <= SREF_EXIT;
+                end
+
+                SREF_EXIT: begin
+                    sdram_cke <= 1'b1;
+                    sref_state <= SREF_WAIT_TXSR;
+                end
+
+                SREF_WAIT_TXSR: begin
+                    if (txsr_end)
+                        sref_state <= SREF_END;
+                end
+
+                SREF_END: begin
+                    sref_state <= SREF_IDLE;
+                end
+
+                default: begin
+                    sref_state <= SREF_IDLE;
+
+                    sdram_cke <= 1'b1;
+                    sref_cmd_out <=  CMD_NOP;
+                    sref_bank_out <= 2'b11;
+                    sref_addr_out <= 12'hfff;
+                end
+            endcase
+        end
+    end
+
+    assign sref_end = (sref_state == SREF_END);
+    
+endmodule
---
+++ sdram_self_ref_tb.v
@@ -0,0 +1,79 @@
+`timescale 1ns / 1ps
+
+module sdram_self_ref_tb ();
+ 
+    reg             sys_clk = 0;
+    reg             sys_reset_n = 0;
+    reg             sref_en = 0;
+    
+    wire            sdram_cke;
+    wire [3:0]      sdram_cmd;
+    wire [1:0]      sdram_ba;
+    wire [11:0]     sdram_addr;
+    wire            self_ref_done;
+    
+    wire [15:0]     sdram_dq;
+    wire            init_done;
+    
+    assign init_done = 1;
+    
+    // Generate 100MHz clock (10ns period)
+    always #5 sys_clk = ~sys_clk;
+
+    initial begin
+        $dumpfile("sdram_self_ref.vcd");
+        $dumpvars(0);
+    end
+ 
+    // Reset Sequence
+    initial begin
+        sys_reset_n = 1'b0;
+        repeat(5) @(posedge sys_clk);
+        sys_reset_n = 1'b1;
+    end
+ 
+    // Trigger Self-Refresh
+    initial begin
+        repeat(10) @(posedge sys_clk);
+        sref_en = 1'b1; // Enable Self-Refresh
+        repeat(15) @(posedge sys_clk);
+        sref_en = 1'b0; // Disable Self-Refresh (Exit)
+    end
+ 
+    // Monitor Self-Refresh Completion
+    initial begin
+        @(posedge self_ref_done);
+        $display("Self-Refresh Completed!");
+        repeat (5) @(posedge sys_clk);
+        $finish;
+    end
+ 
+    // Instantiate Self-Refresh Module
+    sdram_self_ref_gen  sdram_self_refresh_inst (
+        .sys_clk        (sys_clk),
+        .sys_reset_n    (sys_reset_n),
+        .sref_en        (sref_en),
+        .init_done      (init_done),
+        .sdram_cke      (sdram_cke),
+        .sref_cmd_out   (sdram_cmd),
+        .sref_bank_out  (sdram_ba),
+        .sref_addr_out  (sdram_addr),
+        .sref_end       (self_ref_done)
+    );
+ 
+    // Instantiate SDRAM Model
+    sdram_model_plus  sdram_model_plus_inst (
+        .Dq         (sdram_dq),        // Bi-directional Data Bus
+        .Addr       (sdram_addr),
+        .Ba         (sdram_ba),
+        .Clk        (sys_clk),
+        .Cke        (sdram_cke),
+        .Cs_n       (sdram_cmd[3]),
+        .Ras_n      (sdram_cmd[2]),
+        .Cas_n      (sdram_cmd[1]),
+        .We_n       (sdram_cmd[0]),
+        .Dqm        (2'b00),
+        .Debug      (1'b1)
+    );
+ 
+endmodule
